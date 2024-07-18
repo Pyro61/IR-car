a2478a54126286c5432587b825617c20a357c2d3
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2024-07-18 18:48:02 +0200

    ws2812b: refactoring and adding comments

--- Core/light/ws2812b/ws2812b.c
+++ Core/light/ws2812b/ws2812b.c
@@ -1,10 +1,14 @@
+/* Module header include */
 #include "ws2812b.h"
+
+/* Board hardware includes */
 #include "spi.h"
 #include "tim.h"
 
+/* Standard library includes */
 #include <string.h>
 
-
+/* Defines */
 /* 0 and 1 bit codes */
 #define ZERO						0x06
 #define ONE							0x1E
@@ -13,19 +17,22 @@
 #define BITS_IN_LED					24
 #define RESET_SIZE					60
 #define ONE_LIGHT_SIZE 				(LEDS_IN_LIGHT * BITS_IN_LED)
-//#define BUF_SIZE					(LIGHTS_AMOUNT * LEDS_IN_LIGHT * BITS_IN_LED) + RESET_SIZE
 #define BUF_SIZE					((LIGHTS_AMOUNT * ONE_LIGHT_SIZE) + RESET_SIZE)
 
+/* Macros */
 /* Calculating position in buffer to write bit macro */
 #define BUF_POSITION(light,led)		(RESET_SIZE + (light * ONE_LIGHT_SIZE) + (led * BITS_IN_LED))
 
 /* Checking last bit of x and writing 0/1 bit code macro */
 #define CHECKBIT(x)					(x && 0x01) ? ONE : ZERO
 
-
+/* Variables */
 static uint8_t buf[BUF_SIZE] = {0};
+
 static bool is_buf_during_sending = false;
 
+/* Function definitions */
+/* Writing into buffer function */
 void ws2812b_code(uint8_t r, uint8_t g, uint8_t b, uint8_t light, uint8_t led)
 {
 	if (light < 0 || light > (LIGHTS_AMOUNT - 1)|| led < 0 || led > (LEDS_IN_LIGHT - 1))
@@ -45,12 +52,14 @@ void ws2812b_code(uint8_t r, uint8_t g, uint8_t b, uint8_t light, uint8_t led)
 }
 
 
+/* Checking transmit possibility function */
 bool ws2812b_is_transmit_possible(void)
 {
 	return is_buf_during_sending;
 }
 
 
+/* Sending buffer to LEDs function */
 void ws2812b_send(void)
 {
 	is_buf_during_sending = true;
@@ -58,16 +67,21 @@ void ws2812b_send(void)
 }
 
 
+/* End of SPI transmit function */
 void ws2812b_end_of_transmit(void)
 {
+	/* Setting coding bytes to ZERO (for LEDs) */
 	memset(&buf[RESET_SIZE], ZERO, sizeof(buf) - (RESET_SIZE));
+
 	is_buf_during_sending = false;
 }
 
 
+/* Initializing the lamps with blank color function */
 void ws2812b_init(void)
 {
+	/* Setting coding bytes to ZERO (for LEDs) */
 	memset(&buf[RESET_SIZE], ZERO, sizeof(buf) - (RESET_SIZE));
+
 	ws2812b_send();
 }
-
--- Core/light/ws2812b/ws2812b.h
+++ Core/light/ws2812b/ws2812b.h
@@ -1,25 +1,27 @@
 #pragma once
 
+/* Standard library includes */
 #include <stdint.h>
 #include <stdbool.h>
 
-
+/* Defines */
 #define LIGHTS_AMOUNT	4
 #define LEDS_IN_LIGHT	7
 
-/* Check whether the transmission is in progress */
+/* Function prototypes */
+/* Check whether the transmission is in progress function */
 bool ws2812b_is_transmit_possible(void);
 
-/* Coding the RGB code to the right LED function */
+/* Coding the RGB code to the correct LED function */
 void ws2812b_code(uint8_t r, uint8_t g, uint8_t b, uint8_t light, uint8_t led);
 
 /* Sending the RGB codes to LEDs function */
 void ws2812b_send(void);
 
-/* End of transmit function */
+/* End of RGB codes transmit function */
 void ws2812b_end_of_transmit(void);
 
-/* Initializing the lamps with blank color function */
+/* Init function */
 void ws2812b_init(void);
 
 
